a6ca6fc3352d3dc75f474064e2c3f7b110380814
Giga <gigabyte5671@gmail.com>
2024-12-07 21:50:36 +1300

    Update `getPuzzleInput()`
    - Trim the input.
    - Save the input to disk.

--- 2024/js/getPuzzleInput.mjs
+++ 2024/js/getPuzzleInput.mjs
@@ -1,13 +1,18 @@
-import { readFileSync } from 'node:fs';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
 import { session } from './session.mjs';
 
 export async function getPuzzleInput (day, test) {
+	const path = `../inputs/day${day.toString().padStart(2, '0')}-${test ? 'test' : 'input'}.txt`;
 	if (test) {
-		const path = `../inputs/day${day.toString().padStart(2, '0')}-test.txt`;
-		return readFileSync(path, { encoding: 'utf-8' }).replaceAll('\r', '');
+		return readFileSync(path, { encoding: 'utf-8' }).replaceAll('\r', '').trim();
+	}
+	if (existsSync(path)) {
+		return readFileSync(path, { encoding: 'utf-8' }).replaceAll('\r', '').trim();
 	}
 	const headers = { cookie: session };
 	const url = `https://adventofcode.com/2024/day/${day}/input`;
 	const result = await fetch(url, { headers });
-	return await result.text();
+	const text = await result.text();
+	writeFileSync(path, text, { encoding: 'utf-8' });
+	return text.trim();
 }
